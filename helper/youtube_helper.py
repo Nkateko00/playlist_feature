0afd550dea34d02b113c4393dc4b1fab3afdcd18
Nkateko Nkuna <tekon@Binary-NN.vodacom.corp>
2024-02-13 23:28:22 +0200

    adding sleeps for now

--- helper/youtube_helper.py
+++ helper/youtube_helper.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from time import *
+import time
 from xpath_paths import XPaths
 
 #Xpaths here for now will fix
@@ -15,32 +15,37 @@ class youtubeHelper:
 
     def youtube_homepage(context):
         context.driver = webdriver.Chrome()
-        context.driver.implicitly_wait(15)
+        context.driver.implicitly_wait(10)
         #allow elements to be found on dom
         context.driver.get(youtube_url)
         
     def click_search_bar(context):
         search_bar = context.driver.find_element(By.XPATH,input_search)
+        time.sleep(5) 
         search_bar.click()
         
     def input_into_search_bar(context,search):
         input_search_bar = context.driver.find_element(By.XPATH,input_search)
         input_search_bar.clear() #clear any existing text
         input_search_bar.send_keys(search)
+        time.sleep(5) 
         input_search_bar.submit()
         
     def click_playlist(context):
         playlist_displayed = context.driver.find_element(By.XPATH,click_playlist)
+        time.sleep(5) 
         playlist_displayed.click()
         
     def click_song(context):
         play_song = context.driver.find_element(By.XPATH,click_song) 
+        time.sleep(5) 
         #  //*[@id="video-title"] //*[@id="text"]
         play_song.click()
         # allow song to play for duration & quit driver
 
     def validate_search_results(context,expected_outcome):
         search_outcome = context.driver.find_element(By.XPATH,playing_song)
+        time.sleep(5) 
         final_outcome = search_outcome.text
         #unable to loop through element so text
         #loop through results & return true if there is text else return false
